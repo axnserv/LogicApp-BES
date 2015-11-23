04505c23f9d2d2f04ff1e27a79cf37e369c40326
Kaden Mai <v-kamai@microsoft.com>
2015-11-23 13:11:41 -0800

    update for LogicApp team
    + Edit apiapp.json
    + enable UI
    + test

--- Azure ML Connector/AzureMLConnector/App_Start/SwaggerConfig.cs
+++ Azure ML Connector/AzureMLConnector/App_Start/SwaggerConfig.cs
@@ -176,9 +176,9 @@ namespace AzureMLConnector
                         //c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
                         // ***** Uncomment the following to enable the swagger UI *****
                         
-                        //    })
-                        //.EnableSwaggerUi(c =>
-                        //    {
+                            })
+                        .EnableSwaggerUi(c =>
+                            {
                         
                         // Use the "InjectStylesheet" option to enrich the UI with one or more additional CSS stylesheets.
                         // The file must be included in your project as an "Embedded Resource", and then the resource's
